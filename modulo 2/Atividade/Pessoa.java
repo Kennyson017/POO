2eb0e241b5d6e8ee22bf2101baac81b3a79673b0
kennyson017 <kennyson.florencio@pditabira.com>
2025-06-08 11:42:03 -0300

    Update

--- modulo 2/Atividade/Pessoa.java
+++ modulo 2/Atividade/Pessoa.java
@@ -1,10 +1,18 @@
 public class Pessoa {
     private String nome;
     private int idade;
+    private String nacionalidade;
 
+    // CONTRUTOR DE USUARIO
     public Pessoa(String nome, int idade) {
         this.nome = nome;
         this.idade = idade;
+    }
+
+    // CONTRUTOR DE AUTOR
+    public Pessoa(String nome, String nacionalidade) {
+        this.nome = nome;
+        this.nacionalidade = nacionalidade;
     }
 
     public String getNome() {
@@ -15,12 +23,20 @@ public class Pessoa {
         return idade;
     }
 
+    public String getNacionalidade() {
+    return nacionalidade;
+    }
+
     public void setNome(String nome) {
         this.nome = nome;
     }
 
     public void setIdade(int idade) {
         this.idade = idade;
+    }
+
+    public void setNacionalidade(String nacionalidade) {
+        this.nacionalidade = nacionalidade;
     }
 
     public void apresentar() {
